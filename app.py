d12a6eab7de266c63b8fb8608e3a6d342a901479
UnknownLearn <kingnepalsaroj@gmail.com>
2022-02-16 16:34:42 +0545

    code optimization

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, Response, request, jsonify, redirect, url_for
 import cv2
 import numpy as np
 import keras
+from PIL import Image, ImageOps
 
 
-def character_recog(image_path):
+def character_recog(gray):
     img_arr = []
-    gray = cv2.imread('new.jpg', cv2.IMREAD_GRAYSCALE)
-    #gray = cv2.imread("xc.jpg", cv2.COLOR_BGR2GRAY)
-    reteval, img = cv2.threshold(
-        gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    img = cv2.resize(img, (32, 32), interpolation=cv2.INTER_AREA)
-    img = np.reshape(np.array(img), (32, 32, 1))
-    cv2.imwrite("check.jpg", img)
-    img_arr.append(img)
+    img_arr.append(gray)
     img_arr = np.array(img_arr)
     model = keras.models.load_model("nmodelgi0")
     result = model.predict(img_arr)
@@ -25,9 +19,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def loadImage():
-    temp_file = request.files['image']
-    temp_file.save("new.jpg")
-    return jsonify({'message': character_recog(temp_file.filename)})
+    img = Image.open(request.files['image'])
+    newsize = (32, 32)
+    img = img.resize(newsize)
+    img = ImageOps.grayscale(img)
+    img = np.array(img)
+    img = np.reshape(img, (32, 32, 1))
+    return jsonify({'message': character_recog(img)})
 
 
 if __name__ == "__main__":
--- use_to_check_the_data.py
+++
@@ -1,6 +0,0 @@
-
-import requests
-
-files = {'files': open('a.jpg','rb')}
-eval = requests.post("http://127.0.0.1:5000", files = files)
-print(eval.text)
